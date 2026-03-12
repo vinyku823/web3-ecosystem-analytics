266fe694c16e771bc03efdcddd651b109a6c2fa1
pappu chandravanshi <vinaykurmi823@gmail.com>
2026-03-12 20:58:30 +0530

    Create collector.js

---
+++ src/src/collector.js
@@ -0,0 +1,10 @@
+function collectData(){
+
+ const timestamp = new Date().toISOString()
+
+ console.log("Collecting ecosystem analytics data...")
+ console.log("Data collection started at",timestamp)
+
+}
+
+module.exports = { collectData }
