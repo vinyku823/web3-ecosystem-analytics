f54f03203052eb3bdde2b39e23d230d8856bad5c
pappu chandravanshi <vinaykurmi823@gmail.com>
2026-03-13 14:31:05 +0530

    Update tracker.js

--- src/tracker.js
+++ src/tracker.js
@@ -8,3 +8,17 @@ function scanActivity(){
 }
 
 module.exports = { scanActivity }
+// src/tracker.js
+const data = require("../data/projects.json")
+
+function trackByCategory(projects) {
+  const stats = {}
+  projects.forEach(p => {
+    stats[p.category] = (stats[p.category] || 0) + 1
+  })
+  return stats
+}
+
+const categoryStats = trackByCategory(data.projects)
+console.log("📊 Projects by Category:")
+console.table(categoryStats)
